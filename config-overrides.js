ac2ca1908eba7a7cccb374a8b59599dcbd288e69
Abiola <gbozee@gmail.com>
2018-02-27 08:11:19 +0100

    would test locally

--- config-overrides.js
+++ config-overrides.js
@@ -1,26 +1,24 @@
-
 function rewireVueJS(config, env) {
-  config.module.loaders.push({
-    test: /\.vue$/,
-    loader: 'vue'
-  });
-  config.vue = {
-    loaders: {
-      js: 'babel'
-    }
-  }
+  // config.module.loaders.push({
+  //   test: /\.vue$/,
+  //   loader: 'vue'
+  // });
+  // config.vue = {
+  //   loaders: {
+  //     js: 'babel'
+  //   }
+  // }
   config.resolve = {
     alias: {
-      vue: 'vue/dist/vue.js'
+      vue: "vue/dist/vue.js"
     }
-  }
+  };
 
   return config;
 }
 
-
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
-  config = rewireVueJS(config, env)
+  config = rewireVueJS(config, env);
   return config;
 };
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const App = () => (
   <div style={styles}>
     <Hello name="CodeSandbox" />
     <h2>Start editing to see some magic happen {'\u2728'}</h2>
-    <VueComponent />
+    {/*<VueComponent />*/}
   </div>
 );
 
